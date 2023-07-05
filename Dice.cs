fcc4366b908a9d1ecb9010c0f24c00f06495e065
JakubKaras <karas.kuba@email.cz>
2023-07-05 20:04:41 +0200

    class containing Dice info -- number of sides of dice and number of dice -- and roll function added

---
+++ Dice.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleDiceCounter
+{
+    internal class Dice
+    {
+        private int numberOfSides;
+        private int numberOfDice;
+
+        public Dice(int numberOfSides, int numberOfDice)
+        {
+            this.numberOfSides = numberOfSides;
+            this.numberOfDice = numberOfDice;
+        }
+
+        public int roll()
+        {
+            Random rnd = new Random();
+            int roll = 0;
+            for (int i = 0; i < numberOfDice; i++)
+                roll += rnd.Next(1, numberOfSides + 1);
+            return roll;
+        }
+    }
+}
