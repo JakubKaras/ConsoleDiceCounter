8b13a9ec7fd43185bdfb75b81e634c117715c939
JakubKaras <karas.kuba@email.cz>
2023-07-22 13:20:15 +0200

    a game is simulated

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace ConsoleDiceCounter
 {
@@ -10,6 +11,20 @@ namespace ConsoleDiceCounter
     {
         static void Main(string[] args)
         {
+            Dice dice = new Dice(6, 2);
+            List<Player> players = new List<Player>
+            {
+                new Player("wheee"),
+                new Player("blahblah")
+            };
+            Game gameWithoutDice = new Game(players, 2);
+            Game gameWithDice = new Game(players, 2, dice);
+            for (int i = 0; i < 4; i++)
+            {
+                gameWithoutDice.PlayTurn();
+                Console.WriteLine(gameWithoutDice.ToString());
+            }
+            Console.ReadLine();
         }
     }
 }
